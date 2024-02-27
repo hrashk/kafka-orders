e7c4b8d3b7690b6cff53f621e16fc3dee8f04028
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-02-27 13:09:59 +0100

    fix the controller to send an event to kafka

--- order-service/src/main/java/io/github/hrashk/order/service/web/OrderController.java
+++ order-service/src/main/java/io/github/hrashk/order/service/web/OrderController.java
@@ -1,15 +1,28 @@
 package io.github.hrashk.order.service.web;
 
+import io.github.hrashk.order.service.broker.OrderEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/orders")
 public class OrderController {
+    @Value("${app.kafka.order.topic}")
+    private String topicName;
+
+    private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
     @PostMapping
-    public ResponseEntity<OrderResponse> sendOrderEvent() {
+    public ResponseEntity<OrderResponse> sendOrderEvent(@RequestBody OrderEvent event) {
+        kafkaTemplate.send(topicName, event);
+        
         return ResponseEntity.ok(new OrderResponse("Message sent"));
     }
 }
